8b3b7f07308f22e9f9b96e4f1794b55714c85a8b
sawerrai-AU <your-email@domain.com>
2025-12-13 01:05:28 -0500

    Fix downloads page and public file listing

--- frontend/src/pages/Downloads.js
+++ frontend/src/pages/Downloads.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 function Downloads() {
   // Store public files in state
   const [files, setFiles] = useState([]);
@@ -15,7 +15,7 @@ function Downloads() {
       const res = await axios.get("http://localhost:5000/api/publicfiles");
       setFiles(res.data);
     } catch (err) {
-      console.log(err);
+      console.error(err);
       alert("Could not load files");
     }
   };
@@ -25,25 +25,28 @@ function Downloads() {
       <h2>Downloads</h2>
 
       {/* Display list of public files */}
-      {files.length > 0 ? (
+      {files.length == 0 ? (
+        <p> No public files available </p>
+      ):(
         <ul>
-          {files.map((file, index) => (
-            <li key={index}>
+          {files.map((file) => (
+            <li key={file._id}>
               {/* Show file name */}
-              {file.filename}
+              {file.originalName}
 
               {/* Download button */}
               <a
                 href={`http://localhost:5000/uploads/${file.filename}`}
                 style={{ marginLeft: "10px" }}
+                download
               >
                 Download
               </a>
             </li>
           ))}
         </ul>
-      ) : (
-        <p>No files found</p>
+      
+        
       )}
     </div>
   );
