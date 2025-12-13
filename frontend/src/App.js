eabd9da7dfe89ee1d4ee4de9ceda2412adeb8790
sawerrai-AU <your-email@domain.com>
2025-12-12 22:55:43 -0500

    Connect frontend pages using React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import{BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Rrgister";
+import Upload from "./pages/Upload";
+import Downloads from "./pages/Downloads";
+import MyFiles from "./pages/MyFiles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path ="/login" element = {<Login/>}/>
+      <Route path = "/register" element = {<Register/>} />
+      <Route path = "/upload" element = {<Upload />} />
+      <Route path = "/downloads" element = {<Downloads/>} />
+      <Route path ="/myfiles" element = {<MyFiles/>} />
+    </Routes>
+    </BrowserRouter>
   );
+  
 }
 
 export default App;
