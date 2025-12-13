f5c4364b3852fcd6146c78ddea7771baa8703feb
sawerrai-AU <your-email@domain.com>
2025-12-13 00:43:36 -0500

    Fix register page and form handeling

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function Register() {
+  //state to store form inputs
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+const[password, setPassword] = useState(" ");
 
+//Handle form submit
   const handleRegister = async (e) => {
     e.preventDefault();
 
     try {
+      //send registration request to backend
       const res = await axios.post("http://localhost:5000/api/register", {
         username: username,
         email: email,
+        password:password,
       });
 
-      alert(res.data.msg);
-      window.location.href = "/loginn";
+      alert(res.data.msg || "Registration successful");
+      //Redirect to Login page
+      window.location.href = "/login";
     } catch (err) {
       console.log(err);
       alert("Registration failed!!");
@@ -29,19 +36,31 @@ function Register() {
         <input
           type="text"
           placeholder="Username"
+          value={username}
           onChange={(e) => setUsername(e.target.value)}
+          required
         /><br/>
 
+{/*Email*/}
         <input
           type="email"
           placeholder="Email"
-        /><br/>
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}
+          required
+          />
+        <br/>
 
+{/*password*/}
         <input
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}
+          required
         /><br/>
 
+{/*submit*/}
         <button type="submit">Register</button>
       </form>
     </div>
